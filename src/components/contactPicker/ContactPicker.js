b9f7b1f1f10880587c8d621a4815d66b621fec09
guntza10 <jomphop.saibuatong@hotmail.com>
2024-12-07 23:39:40 +0700

    (update) add required to select

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const ContactPicker = React.memo(({ contacts, value, onChange, name }) => {
   return (
-    <select name={name} value={value} onChange={onChange}>
+    <select name={name} value={value} onChange={onChange} required>
       <option value="">No Contact Selected</option>
       {contacts.map((contact) => (
         <option
