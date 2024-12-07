fc47f2fb910b474992a2224ed4d77ad6f522965f
guntza10 <jomphop.saibuatong@hotmail.com>
2024-12-07 23:26:22 +0700

    (add) implement ContactPicker

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,19 @@
 import React from "react"
 
-const ContactPicker = React.memo(() => {
-  return <></>
+const ContactPicker = React.memo(({ contacts, value, onChange, name }) => {
+  return (
+    <select name={name} value={value} onChange={onChange}>
+      <option value="">No Contact Selected</option>
+      {contacts.map((contact) => (
+        <option
+          key={contact.id}
+          value={`${contact.name}, ${contact.email}, ${contact.phone}`}
+        >
+          {contact.name}
+        </option>
+      ))}
+    </select>
+  )
 })
 
 export default ContactPicker
